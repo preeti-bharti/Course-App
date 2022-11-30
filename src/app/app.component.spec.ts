4391abe82a4f305f4ee87968d30c06afd82aef78
preeti-bharti <preeti_bharti@epam.com>
2022-11-30 16:43:57 +0530

    resolved comments

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('courses-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('courses-app app is running!');
-  });
 });
